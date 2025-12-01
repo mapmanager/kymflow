42d711534591e6b93c39d02e8d4068c4d6c484a7
Robert Cudmore <robert.cudmore@gmail.com>
2025-12-01 14:14:03 -0500

    before refactoring src/ to src/kymflow/

--- src/kymflow_core/__init__.py
+++
@@ -1,6 +0,0 @@
-from importlib.metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("kymflow")
-except PackageNotFoundError:  # running from source without install
-    __version__ = "0.0.0+local"
